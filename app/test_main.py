c20ac71be1f4d529f7a89749ab54bdd17e3bd84e
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2024-12-16 07:42:02 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,13 +2,17 @@ import pytest
 from app.main import get_coin_combination
 
 
-@pytest.mark.parametrixe(
+@pytest.mark.parametrize(
     "cents, result",
     [
-        pytest.param(1, [1, 0, 0, 0], "test_sould_return_correct_result_1"),
-        pytest.param(6, [1, 1, 0, 0], "test_sould_return_correct_result_2"),
-        pytest.param(17, [2, 1, 1, 0], "test_sould_return_correct_result_3"),
-        pytest.param(50, [0, 0, 0, 2], "test_sould_return_correct_result_4")
+        pytest.param(
+            1, [1, 0, 0, 0], id="test_sould_return_correct_result_1"),
+        pytest.param(
+            6, [1, 1, 0, 0], id="test_sould_return_correct_result_2"),
+        pytest.param(
+            17, [2, 1, 1, 0], id="test_sould_return_correct_result_3"),
+        pytest.param(
+            50, [0, 0, 0, 2], id="test_sould_return_correct_result_4")
     ]
 )
 def test_sould_return_correct_result(cents: int, result: list) -> None:
